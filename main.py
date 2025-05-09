b802cd10b88d54240fcdc50e85c94fe9104bca9d
lasseas <lasseas@stud.ntnu.no>
2025-05-09 12:41:05 +0200

    Endra datafiler for generering av instanser

--- main.py
+++ main.py
@@ -9,27 +9,13 @@ import matplotlib.pyplot as plt
 import platform
 #import psutil
 from pyomo.environ import *
+from _run_everything import excel_path, instance, year, num_branches_to_firstStage, num_branches_to_secondStage, num_branches_to_thirdStage, num_branches_to_fourthStage, num_branches_to_fifthStage, num_branches_to_sixthStage, num_branches_to_seventhStage, num_branches_to_eighthStage, num_branches_to_ninthStage, num_branches_to_tenthStage
 
 #####################################################################################
 ################################## KONSTANTE SETT ###################################
 #####################################################################################
 #################### HUSK Å ENDRE DISSE I DE ANDRE FILENE OGSÅ ######################
 #####################################################################################
-#excel_path = "NO1_Aluminum_2024_combined historical data.xlsx"
-excel_path = "NO1_Pulp_Paper_2024_combined historical data.xlsx"
-instance = 1                    # state which instance you would like to run for
-year = 2025                     # state which year you would like to run for
-
-num_branches_to_firstStage = 2 # Antall grener til det vi i LateX har definert som Omega^first
-num_branches_to_secondStage = 45
-num_branches_to_thirdStage = 45
-num_branches_to_fourthStage = 0
-num_branches_to_fifthStage = 0
-num_branches_to_sixthStage = 0
-num_branches_to_seventhStage = 0
-num_branches_to_eighthStage = 0
-num_branches_to_ninthStage = 0
-num_branches_to_tenthStage = 0
 
 ##################################################################################
 ############################### READING EXCEL FILE ###############################
